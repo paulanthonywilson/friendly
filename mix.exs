8965ce551183966d7d01742728ac7467a4f1677d
Paul Wilson <paulanthonywilson@gmail.com>
2023-03-01 16:55:37 +0000

    fix docs for COMMENTARY rename

--- mix.exs
+++ mix.exs
@@ -57,6 +57,6 @@ defmodule Friendly.MixProject do
   end
 
   defp docs do
-    [main: "readme", extras: ["README.md", "COMMENTRY.md"]]
+    [main: "readme", extras: ["README.md", "COMMENTARY.md"]]
   end
 end
